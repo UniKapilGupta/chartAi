39c81e8a14e443f9d274f3df299550d936a8159f
Kapil Kumar Gupta <kapil.gupta@tigeranalytics.com>
2025-10-09 18:24:50 +0530

    tabs added

--- components/data_loader.py
+++ components/data_loader.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import streamlit as st
 
-def load_csv():
-    uploaded_file = st.sidebar.file_uploader("Upload CSV", type=["csv"])
+def load_csv(uploaded_file):
     if uploaded_file:
-        df = pd.read_csv(uploaded_file, encoding="latin1", on_bad_lines="skip")
-        st.success("File loaded successfully!")
-        return df
+        try:
+            df = pd.read_csv(uploaded_file, encoding="latin1", on_bad_lines="skip")
+            st.success("File loaded successfully!")
+            return df
+        except Exception as e:
+            st.error(f"Error loading file: {e}")
+            return None
     else:
         st.info("Please upload a CSV file.")
         return None
